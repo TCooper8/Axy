fb50424e4ab5be213344616f3ad50a50ab8715e4
tcooper8 <coopetyl@live.com>
2017-03-25 11:41:50 -0600

    Updated project.

--- Script.fsx
+++ Script.fsx
@@ -8,13 +8,16 @@ open System
 open Axy
 
 module ActorTest =
-  let actor: (int AsyncReplyChannel * int) Actor.ActorEvent Actor = Actor.actorOf {
+  let actor: int Actor.ActorEvent Actor = Actor.actorOf {
     initialState = 5
     onFailed = fun state e ->
       sprintf "Error: %A" e |> ignore
       Actor.Stopped
     receive = fun x -> function
-      | Actor.Notify (chan, y) ->
-        chan.Reply(x)
+      | Actor.Notify (y) ->
         Actor.Running y
   }
+
+  for i in 0 .. 1000000 do
+    actor.Post (Actor.Notify i)
+    printfn "Posted: %A" i
